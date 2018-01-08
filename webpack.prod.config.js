054a4504e76745828fa77c848610da021a8ad968
jcoro <jcoronite@hotmail.com>
2018-01-08 15:00:49 -0700

    add webpack plugin

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -18,5 +18,10 @@ module.exports = {
         path: __dirname + '/dist',
         publicPath: '/',
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
+    ]
 };
